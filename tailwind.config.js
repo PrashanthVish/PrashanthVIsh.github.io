fe3ab008e76b2f2e6a2083692508cb687bddc518
Prashanth Vish <4738905+PrashanthVish@users.noreply.github.com>
2021-01-01 10:49:19 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ['Poppins', 'Arial', 'sans-serif'],
+      body: ['Poppins', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
